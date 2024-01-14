407269241ea21794c000d611337855d5d3e09e07
cjh-19 <gns8812@naver.com>
2024-01-15 01:55:18 +0900

    인증 수정
    access token 활용

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/oauth2/service/CustomOAuth2AuthService.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/oauth2/service/CustomOAuth2AuthService.java
@@ -23,12 +23,22 @@ public class CustomOAuth2AuthService implements OAuth2UserService<OAuth2UserRequ
     @SneakyThrows
     @Override
     public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
-        log.info("CustomOAuth2AuthService");
-        OAuth2UserService delegate = new DefaultOAuth2UserService();
+//        log.info("CustomOAuth2AuthService");
+//        OAuth2UserService delegate = new DefaultOAuth2UserService();
+//        OAuth2User oAuth2User = delegate.loadUser(request);
+//        String registrationId = request.getClientRegistration().getRegistrationId();
+//        String userNameAttributeName = request.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+//        OAuth2Attributes attributes = OAuth2Attributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+//        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")), attributes.getAttributes(), attributes.getNameAttributeKey());
+
+        /*수정*/
+        // 클라이언트로부터 전달받은 access token을 사용하여 사용자 정보 로드
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(request);
-        String registrationId = request.getClientRegistration().getRegistrationId();
-        String userNameAttributeName = request.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
-        OAuth2Attributes attributes = OAuth2Attributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")), attributes.getAttributes(), attributes.getNameAttributeKey());
+        // 여기에서 필요한 추가 로직을 구현
+        // 예를 들어, 사용자 정보를 기반으로 데이터베이스에 사용자가 있는지 확인하거나, 새로운 사용자를 등록할 수 있습니다.
+
+        // OAuth2User 반환
+        return oAuth2User;
     }
 }
