86a255f966a8978590430df49baddf60e4b347d4
cjh-19 <gns8812@naver.com>
2024-02-26 15:13:17 +0900

    주석 추가

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/repository/UserRepository.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/repository/UserRepository.java
@@ -3,6 +3,8 @@ package unknownnote.unknownnoteserver.repository;
 import unknownnote.unknownnoteserver.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+// entity를 가지고 db테이블에 접근할 repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> { // 해당하는 entity 클래스와 id 타입인 long
+    // findByUsername 커스텀 쿼리를 통해 내부에 username을 전달
     UserEntity findByUsername(String username);
 }
