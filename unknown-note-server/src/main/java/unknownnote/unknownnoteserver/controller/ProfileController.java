6fae9aa84b7c45d619214be25f83d26446a2abda
Minhyung Park <112764586+minibr@users.noreply.github.com>
2024-05-12 12:32:40 +0900

    Update ProfileController.java

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/controller/ProfileController.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/controller/ProfileController.java
@@ -52,4 +52,55 @@ public class ProfileController {
             return ResponseEntity.ok(errorService.setError(2000,"유효하지 않은 접근입니다"));
         }
     }
+    @PostMapping("/image")
+    public ResponseEntity<?> uploadFile(@RequestHeader("Authorization") String jwtToken, @RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseEntity.status(4000).body(new ApiResponse(4000, "No file uploaded", null));
+        }
+        try {
+            // 파일 이름 생성 (클라이언트 파일 이름을 사용하거나 UUID를 생성하여 사용할 수 있음)
+            String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+            Path filePath = Paths.get(DIRECTORY, fileName);
+            // 파일 저장
+            Files.copy(file.getInputStream(), filePath);
+
+            String fileDownloadUri = "http://localhost:8000/profile/files/" + fileName;
+            return ResponseEntity.ok(new ApiResponse(1000, "File uploaded successfully", fileDownloadUri));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(4000).body(new ApiResponse(4000, "Could not upload the file", null));
+        }
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        try {
+            Path file = Paths.get(DIRECTORY).resolve(filename);
+            if (!Files.exists(file)) {
+                return ResponseEntity.notFound().build();
+            }
+            Resource fileResource = new UrlResource(file.toUri());
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileResource.getFilename() + "\"")
+                    .body(fileResource);
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+    // ApiResponse 클래스
+    @Getter
+    @Setter
+    public class ApiResponse {
+        private int code;
+        private String message;
+        private String data;
+
+        public ApiResponse(int code, String message, String data) {
+            this.code = code;
+            this.message = message;
+            this.data = data;
+        }
+    }
 }
