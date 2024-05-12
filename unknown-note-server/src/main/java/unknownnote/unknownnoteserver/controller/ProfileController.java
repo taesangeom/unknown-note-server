fd5b841881b074985e4286db498bf705c3620603
Minhyung Park <112764586+minibr@users.noreply.github.com>
2024-05-12 12:33:17 +0900

    Update ProfileController.java

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/controller/ProfileController.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/controller/ProfileController.java
@@ -1,11 +1,23 @@
 package unknownnote.unknownnoteserver.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import unknownnote.unknownnoteserver.dto.MyProfileResponse;
 import unknownnote.unknownnoteserver.dto.UserInfoResponse;
 import unknownnote.unknownnoteserver.service.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 
 @RestController
 @RequestMapping("/profile")
