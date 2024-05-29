51a755424fccf3b3b111e430a7920c20c5a42e74
cjh-19 <gns8812@naver.com>
2024-05-29 22:14:38 +0900

    Update JwtService.java
    1. jwt 만료 확인 메소드 추가
    2. secretkey 가져오는 코드 수정

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/jwt/JwtService.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/jwt/JwtService.java
@@ -11,20 +11,35 @@ import org.springframework.stereotype.Service;
 import java.nio.charset.StandardCharsets;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    @Value("${spring.jwt.secret}")
-    private String SECRET_KEY; //비밀 키
+    private SecretKey secretKey;
+
+    public JwtService(@Value("${spring.jwt.secret}")String secret) {
+
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
 
     public int getUserIdFromJwt(String jwtToken){
-        SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
-
         return Jwts.parser()
-                .verifyWith(key)
+                .verifyWith(secretKey)
                 .build().parseSignedClaims(jwtToken)
                 .getPayload().get("user_id", Integer.class);
+    }
+
+    // JWT의 만료 여부를 확인하는 메서드
+    // JWT가 만료되었다면 true를, 아직 유효하다면 false를 반환
+    public Boolean isExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration() // paload에서 만료 시간을 가져옴
+                .before(new Date()); // 현재 시간이 JWT의 만료 시간보다 이후인지 확인
     }
 }
