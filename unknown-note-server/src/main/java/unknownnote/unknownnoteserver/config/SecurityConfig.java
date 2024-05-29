29896beb54f7a53bae8c78c196f66a724032d532
cjh-19 <gns8812@naver.com>
2024-05-29 22:10:15 +0900

    Update SecurityConfig.java
    1. JWTFilter에 따른 security 설정 추가
    2. 경로별 인가 작업 단순화 -> 어차피 권한을 나누지 않으므로 모두에게 열어둠

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/config/SecurityConfig.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/config/SecurityConfig.java
@@ -7,10 +7,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import unknownnote.unknownnoteserver.jwt.JWTFilter;
+import unknownnote.unknownnoteserver.jwt.JwtService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtService jwtService;
+
+    public SecurityConfig(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,16 +36,17 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
+        //JWTFilter 추가
+        // JWTFilter를 UsernamePasswordAuthenticationFilter 클래스 이전에 추가하라는 의미
+        // 이렇게 하면, JWT 토큰을 사용하는 요청이 들어올 때 JWTFilter에서 먼저 토큰을 검증하고,
+        // 이후에 UsernamePasswordAuthenticationFilter에서 사용자 이름과 비밀번호를 추출하여 인증을 시도
+        // 비밀번호는 없으므로 null 값으로 들어감. user_id로 인증
+        http
+                .addFilterBefore(new JWTFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
+
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        /*.requestMatchers(HttpMethod.POST, "/signin").permitAll() // 로그인 경로
-                        .requestMatchers(HttpMethod.GET, "/providers/**").authenticated() // 제공업체로부터 정보를 받아오는 경로
-                        .requestMatchers(HttpMethod.GET, "/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/**").permitAll()
-                        .requestMatchers(HttpMethod.PATCH, "/**").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/**").permitAll()
-                        .anyRequest().authenticated()); // 그 외 모든 요청은 인증된 사용자만 접근 가능*/
                         .anyRequest().permitAll()); // 모든 요청에 대해 접근을 허용
 
         //세션 설정 : STATELESS
