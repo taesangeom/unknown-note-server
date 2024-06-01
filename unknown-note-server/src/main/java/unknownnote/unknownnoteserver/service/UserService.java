f812719a6c5181a5014f8a9d1c83607d0a7fae07
DONGGYU LEE <tony7078@naver.com>
2024-06-01 23:31:25 +0900

    Update UserService.java

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/service/UserService.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/service/UserService.java
@@ -12,8 +12,8 @@ import unknownnote.unknownnoteserver.jwt.JWTUtil;
 import unknownnote.unknownnoteserver.repository.UserRepository;
 import unknownnote.unknownnoteserver.util.SocialUserInfoFetcher;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.*;
 import java.sql.Timestamp;
 import java.time.Instant;
 
@@ -61,16 +61,18 @@ public class UserService {
         String jpgFilePath = directoryPath + userId + ".jpg";
         String pngFilePath = directoryPath + userId + ".png";
         String img_name;
+        String img_file;
 
         // 파일 존재 여부 확인
         if (Files.exists(Paths.get(jpgFilePath))) {
             img_name = userId + ".jpg";
+            img_file = "http://13.48.223.79:8080/profile/files" + img_name;
         } else if (Files.exists(Paths.get(pngFilePath))) {
             img_name = userId + ".png";
+            img_file = "http://13.48.223.79:8080/profile/files" + img_name;
         } else {
-            img_name = null;
+            img_file = "";
         }
-        String img_file = "http://13.48.223.79:8080/profile/files/" + img_name;
 
         UserInfo userInfo = new UserInfo(user.getUserId(), user.getNickname(), user.getIntroduction(), img_file);
 
@@ -89,6 +91,29 @@ public class UserService {
         // 사용자 정보 업데이트
         user.setNickname(userInfoRequest.getNickname());
         user.setIntroduction(userInfoRequest.getIntroduction());
+
+        if(userInfoRequest.getProfile_img().isEmpty())
+        {
+            String directoryPath = "./uploads/";
+            String jpgFilePath = directoryPath + user_id + ".jpg";
+            String pngFilePath = directoryPath + user_id + ".png";
+
+            try {
+                // jpg 파일 존재 여부 확인 후 삭제
+                if (Files.exists(Paths.get(jpgFilePath))) {
+                    Files.delete(Paths.get(jpgFilePath));
+                    System.out.println("Deleted: " + jpgFilePath);
+                }
+
+                // png 파일 존재 여부 확인 후 삭제
+                if (Files.exists(Paths.get(pngFilePath))) {
+                    Files.delete(Paths.get(pngFilePath));
+                    System.out.println("Deleted: " + pngFilePath);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         // 변경 사항 저장
         userRepository.save(user);
