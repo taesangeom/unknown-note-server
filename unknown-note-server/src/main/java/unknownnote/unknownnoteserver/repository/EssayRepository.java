e4f457b3bb90ce3a593500db3f8547ed8a795447
Minhyung Park <112764586+minibr@users.noreply.github.com>
2024-06-01 23:29:15 +0900

    Add files via upload

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/repository/EssayRepository.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/repository/EssayRepository.java
@@ -1,36 +1,12 @@
-/*
 package unknownnote.unknownnoteserver.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import unknownnote.unknownnoteserver.entity.Essay;
+import unknownnote.unknownnoteserver.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import unknownnote.unknownnoteserver.entity.Essay;
-import unknownnote.unknownnoteserver.entity.User;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface EssayRepository extends JpaRepository<Essay, Integer> {
-    @Query("SELECT COUNT(e) FROM Essay e WHERE e.user.userId = :userId")
-    int countByUserId(int userId);
-
-    List<Essay> findByUser_UserIdAndEssayTimeBetween(int userId, Timestamp start, Timestamp end);
-    Page<Essay> findByECategoryOrderByEssayTimeDesc(String category, Pageable pageable);
-    List<Essay> findByUser(User user);
-
-    Page<Essay> findByUser_UserId(int userId, Pageable pageable);
-}*/
-
-package unknownnote.unknownnoteserver.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import unknownnote.unknownnoteserver.entity.Essay;
-import unknownnote.unknownnoteserver.entity.User;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -44,8 +20,8 @@ public interface EssayRepository extends JpaRepository<Essay, Integer> {
     List<Essay> findByUser(User user);
 
     @Query("SELECT e FROM Essay e WHERE e.user.userId = :userId")
-    List<Essay> findByUser_UserId(@Param("userId") int userId);
+    Page<Essay> findByUser_UserId(@Param("userId") int userId, Pageable pageable);
 
     @Query("SELECT e FROM Essay e WHERE e.ECategory = :category ORDER BY e.essayTime DESC")
-    List<Essay> findEssaysByCategory(@Param("category") String category);
+    Page<Essay> findEssaysByCategory(@Param("category") String category, Pageable pageable);
 }
