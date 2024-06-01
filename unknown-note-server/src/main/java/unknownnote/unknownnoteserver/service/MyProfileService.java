02e2677fc2a0e0cdfa09d070829b622526e1439e
DONGGYU LEE <tony7078@naver.com>
2024-06-01 23:00:55 +0900

    Update MyProfileService.java

--- unknown-note-server/src/main/java/unknownnote/unknownnoteserver/service/MyProfileService.java
+++ unknown-note-server/src/main/java/unknownnote/unknownnoteserver/service/MyProfileService.java
@@ -86,7 +86,7 @@ public class MyProfileService {
         } else {
             img_name = null;
         }
-        String img_file = "http://localhost:8000/profile/files/" + img_name;
+        String img_file = "http://13.48.223.79:8080/profile/files/" + img_name;
 
         UserInfo userInfo = new UserInfo(user.getUserId(), user.getNickname(), user.getIntroduction(), img_file);
 
